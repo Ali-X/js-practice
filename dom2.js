167b0a7a1ddeb46f51746d9259e6e36ddbb22653
Ali-X <alina240895@yandex.ru>
2018-12-07 21:22:41 +0200

    Add console.log with alert

--- dom2.js
+++ dom2.js
@@ -5,6 +5,7 @@ let isNikolasCome = confirm("Is Nikolas come?");
 var maryGoodMood = "Mary is happy";
 var maryBadMood = "Mary is upset";
 
+//alert
 //Маша любить, коли хтось приходить в гості І сумує, якщо не приходить ніхто
 if (isAngelCome === false && isStephanCome === false && isNikolasCome === false) {
     alert(maryBadMood);
@@ -34,5 +35,37 @@ else if (isAngelCome === false && (isStephanCome || isNikolasCome)) {
 else if (isAngelCome === true && (isStephanCome || isNikolasCome)) {
     alert(maryGoodMood);
 }
+
+//console.log
+//Маша любить, коли хтось приходить в гості І сумує, якщо не приходить ніхто
+if (isAngelCome === false && isStephanCome === false && isNikolasCome === false) {
+    console.log(maryBadMood);
+}
+
+//Якщо прийдуть хлопці і Анжела, то Степан з Миколою будуть сваритись за Анжелу
+else if (isAngelCome === true && isStephanCome === true && isNikolasCome === true) {
+    console.log(maryBadMood);
+}
+
+//Маша з Анжелою подруги. Маша буде рада.
+else if (isAngelCome === true && isStephanCome === false && isNikolasCome === false) {
+    console.log(maryGoodMood);
+}
+
+//А якщо прийдуть тільки хлопці - то вони будуть спілкуватись з Сашою, а Маша зможе спокійно піти дивитись вогник
+else if (isAngelCome === false && isStephanCome === true && isNikolasCome === true) {
+    console.log(maryGoodMood);
+}
+
+//Маша не любить коли приходить хтось один з хлопців.
+else if (isAngelCome === false && (isStephanCome || isNikolasCome)) {
+    console.log(maryBadMood);
+}
+
+//Але якщо прийде Анжела, хлопці переключаються на Анжелу і Маша буде рада
+else if (isAngelCome === true && (isStephanCome || isNikolasCome)) {
+    console.log(maryGoodMood);
+}
+
 
 
--- food.js
+++ food.js
@@ -1,5 +1,6 @@
 var userMoney = prompt("Скільки у вас є грошей?");
 
+//alert
 if (userMoney >= 100) {
     confirm("Ви можете купити БігМакМеню. Бажаєте?");
 } else if (userMoney >= 50 && userMoney < 100) {
@@ -10,3 +11,13 @@ if (userMoney >= 100) {
     alert("Пробачте, в нашому закладі немає страв для вас :(");
 }
 
+//console.log
+if (userMoney >= 100) {
+    confirm("Ви можете купити БігМакМеню. Бажаєте?");
+} else if (userMoney >= 50 && userMoney < 100) {
+    confirm("Ви можете купити Чізбургер і картоплю. Бажаєте?");
+} else if (userMoney >= 20 && userMoney < 50) {
+    confirm("Ви можете купити колу. Бажаєте?");
+} else {
+    console.log("Пробачте, в нашому закладі немає страв для вас :(");
+}
